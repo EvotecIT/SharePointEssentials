7c5df8b3bf8bea56c68ed6e50cce3d25cbc77256
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-01-20 12:42:23 +0100

    Refactor Sync-FilesToSharePoint.ps1 to streamline file retrieval
    - Introduced Get-FilesLocal function for improved file handling
    - Removed redundant code for source file list processing
    - Enhanced clarity by defining relative URLs for target folders

--- Public/Sync-FilesToSharePoint.ps1
+++ Public/Sync-FilesToSharePoint.ps1
@@ -105,6 +105,10 @@
     }
     $TargetFolder = $Library.RootFolder
 
+    $RelativeUrlRoot = $Web.ServerRelativeUrl
+    $RelativeUrlLibrary = $TargetFolder.ServerRelativeUrl
+
+
     # Get the site relative path of the target folder
     If ($web.ServerRelativeURL -eq "/") {
         $TargetFolderSiteRelativeURL = $TargetFolder.ServerRelativeUrl
@@ -112,64 +116,7 @@
         $TargetFolderSiteRelativeURL = $TargetFolder.ServerRelativeURL.Replace($Web.ServerRelativeUrl, [string]::Empty)
     }
 
-    if ($SourceFileList) {
-        $SourceDirectoryPath = $null
-        # Lets get all files from the source folder
-        [Array] $SourceItems = foreach ($Item in $SourceFileList) {
-            # We need to find the shortest path to the files
-            $TempSourceDirectoryPath = [io.path]::GetDirectoryName($Item)
-            if ($null -eq $SourceDirectoryPath) {
-                $SourceDirectoryPath = $TempSourceDirectoryPath
-            } elseif ($SourceDirectoryPath -ne $TempSourceDirectoryPath) {
-                if ($TempSourceDirectoryPath.Length -lt $SourceDirectoryPath.Length) {
-                    $SourceDirectoryPath = $TempSourceDirectoryPath
-                }
-            }
-            try {
-                Get-Item -Path $Item -ErrorAction Stop
-                Get-Item -Path $TempSourceDirectoryPath -ErrorAction Stop
-            } catch {
-                Write-Color -Text "[e] ", "Unable to get file '$Item' from the source file list. Make sure the path is correct and you have permissions to access it." -Color Yellow, Red
-                Write-Color -Text "[e] ", "Error: ", $_.Exception.Message -Color Yellow, Red
-                return
-            }
-        }
-        if ($SourceItems.Count -eq 0) {
-            Write-Color -Text "[e] ", "No files found in the source file list. Please make sure the list is not empty." -Color Yellow, Red
-            return
-        }
-    } else {
-        $SourceDirectoryPath = $SourceFolderPath
-        # Lets get all files from the source folder
-        $getChildItemSplat = @{
-            Path    = $SourceFolderPath
-            Recurse = $trues
-        }
-        if ($Include) {
-            $getChildItemSplat["Include"] = $Include
-        }
-        try {
-            $SourceItems = @(
-                Get-ChildItem -Directory -Path $SourceFolderPath -Recurse -ErrorAction Stop
-                Get-ChildItem @getChildItemSplat -ErrorAction Stop
-            )
-        } catch {
-            Write-Color -Text "[e] ", "Unable to get files from the source folder. Make sure the path is correct and you have permissions to access it." -Color Yellow, Red
-            Write-Color -Text "[e] ", "Error: ", $_.Exception.Message -Color Yellow, Red
-            return
-        }
-    }
-    [Array] $Source = foreach ($File in $SourceItems | Sort-Object -Unique -Property FullName) {
-        # Dates are not the same as in SharePoint, so we need to convert them to UTC
-        # And make sure we don't add miliseconds, as it will cause issues with comparison
-        $Date = $File.LastWriteTimeUtc
-        [PSCustomObject] @{
-            FullName      = $File.FullName
-            PSIsContainer = $File.PSIsContainer
-            TargetItemURL = $File.FullName.Replace($SourceDirectoryPath, $TargetFolderSiteRelativeURL).Replace("\", "/")
-            LastUpdated   = [datetime]::new($Date.Year, $Date.Month, $Date.Day, $Date.Hour, $Date.Minute, $Date.Second)
-        }
-    }
+    [Array] $Source = Get-FilesLocal -SourceFileList $SourceFileList -SourceFolderPath $SourceFolderPath -Include $Include
 
     Write-Color -Text "[i] ", "Total items in source: ", "$($Source.Count)" -Color Yellow, White, Green
     Write-Color -Text "[i] ", "Total items in target: ", "$($Library.Itemcount)" -Color Yellow, White, Green
