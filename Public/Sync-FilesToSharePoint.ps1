487c898b937f8a1e770b30ef544dc4da0729e69a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-01-21 11:02:36 +0100

    Refactor Sync-FilesToSharePoint.ps1 to use Find-TargetFolder function
    - Replaced inline folder search logic with Find-TargetFolder function for improved readability and maintainability.
    - Simplified the process of locating the target folder in SharePoint libraries.

--- Public/Sync-FilesToSharePoint.ps1
+++ Public/Sync-FilesToSharePoint.ps1
@@ -104,24 +104,8 @@
         return
     }
 
-    $TargetFolder = $null
-    if ($TargetLibraryName -like "*/*") {
-        $Path = $TargetLibraryName -split "/"
-        $TargetLibraryPath = $Path[0]
-        $ListOfSharePointFolders = Get-PnPFolderItem -ItemType Folder -Recursive -FolderSiteRelativeUrl $TargetLibraryPath #| Select-Object Name, ServerRelativeUrl, ItemCount
-        foreach ($Folder in $ListOfSharePointFolders) {
-            $FolderFound = $Folder.ServerRelativeUrl.Replace($Web.ServerRelativeURL, [string]::Empty)
-            if ($FolderFound.TrimStart("/") -eq $TargetLibraryName.TrimStart("/")) {
-                $TargetFolder = $Folder
-                break
-            }
-        }
-    } else {
-        $TargetFolder = $Library.RootFolder
-    }
-
+    $TargetFolder = Find-TargetFolder -TargetLibraryName $TargetLibraryName -Library $Library -Web $Web
     if (-not $TargetFolder) {
-        Write-Color -Text "[e] ", "Unable to find folder ", $TargetLibraryName, " in the library. Please make sure the folder exists." -Color Yellow, Red
         return
     }
 
